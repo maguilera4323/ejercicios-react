b030cbf854e48fe3fdd3d30a6bffaa209dd55ed2
karimhernandez233 <karimhernandez233@gmail.com>
2024-05-23 02:04:31 +0000

    agregados archivos para api

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import CrudApp from "./components/CrudApp"
+import CrudApi from "./components/CrudApi"
 import React from "react"
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
   return (
     <>
       <h1>Ejercicios React</h1>
+      <CrudApi />
+      <hr />
       <CrudApp/>
     </>
   )
